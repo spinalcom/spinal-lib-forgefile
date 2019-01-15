58642bbc07f1ab785167e125a2c8efd65c56e1f2
laurent <htd.laurent@gmail.com>
2019-01-15 16:29:11 +0100

    update type

--- forgefile.d.ts
+++ forgefile.d.ts
@@ -37,16 +37,22 @@ declare module "spinal-lib-forgefile" {
   }
 
   export class ForgeFileDerivativesItem extends Model {
+    name: spinal.Str;
+    path: spinal.Str;
     constructor(params?: ForgeFileDerivativesItemParams);
   }
 
   export class ForgeFileItem extends Model {
     _name: spinal.Str;
-    _name: spinal.Bool;
+    _viewable: spinal.Bool;
     _children: spinal.Lst<ForgeFileDerivativesItem>;
     name: spinal.Str;
     filepath: spinal.Path;
     state: spinal.Choice;
+    urn: spinal.Str;
+    ask_token: spinal.Bool;
+    oauth: spinal.Str;
+    bucket_key: spinal.Str;
     constructor(name: string);
     add_child(child: ForgeFileDerivativesItem): void;
     accept_child(ch: any): boolean;
